e0fe5fe3af38192ddd8d21a483fef1d078aaf03b
malikmuhammadshehzad <malikshehzad1534@gmail.com>
2022-12-30 07:51:43 +0500

    first commit

---
+++ pure-snow.js
@@ -0,0 +1,157 @@
+let snowflakesCount = 200; // Snowflake count, can be overwritten by attrs
+let baseCss = ``;
+
+
+// set global attributes
+if (typeof SNOWFLAKES_COUNT !== 'undefined') {
+  snowflakesCount = SNOWFLAKES_COUNT;
+}
+if (typeof BASE_CSS !== 'undefined') {
+  baseCss = BASE_CSS;
+}
+
+let bodyHeightPx = null;
+let pageHeightVh = null;
+
+function setHeightVariables() {
+  bodyHeightPx = document.body.offsetHeight;
+  pageHeightVh = (100 * bodyHeightPx / window.innerHeight);
+}
+
+// get params set in snow div
+function getSnowAttributes() {
+  const snowWrapper = document.getElementById('snow');
+  if (snowWrapper) {
+    snowflakesCount = Number(
+      snowWrapper.attributes?.count?.value || snowflakesCount
+    );
+  }
+}
+
+// This function allows you to turn on and off the snow
+function showSnow(value) {
+  if (value) {
+    document.getElementById('snow').style.display = "block";
+  }
+  else {
+    document.getElementById('snow').style.display = "none";
+  }
+}
+
+// Creating snowflakes
+function spawnSnow(snowDensity = 200) {
+  snowDensity -= 1;
+
+  for (let i = 0; i < snowDensity; i++) {
+    let board = document.createElement('div');
+    board.className = "snowflake";
+
+    document.getElementById('snow').appendChild(board);
+  }
+}
+
+// Append style for each snowflake to the head
+function addCss(rule) {
+  let css = document.createElement('style');
+  css.appendChild(document.createTextNode(rule)); // Support for the rest
+  document.getElementsByTagName("head")[0].appendChild(css);
+}
+
+// Math
+function randomInt(value = 100) {
+  return Math.floor(Math.random() * value) + 1;
+}
+
+function randomIntRange(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+// Create style for snowflake
+function spawnSnowCSS(snowDensity = 200) {
+  let snowflakeName = "snowflake";
+  let rule = baseCss;
+
+  for (let i = 1; i < snowDensity; i++) {
+    let randomX = Math.random() * 100; // vw
+    let randomOffset = Math.random() * 10 // vw;
+    let randomXEnd = randomX + randomOffset;
+    let randomXEndYoyo = randomX + (randomOffset / 2);
+    let randomYoyoTime = getRandomArbitrary(0.3, 0.8);
+    let randomYoyoY = randomYoyoTime * pageHeightVh; // vh
+    let randomScale = Math.random();
+    let fallDuration = randomIntRange(10, pageHeightVh / 10 * 3); // s
+    let fallDelay = randomInt(pageHeightVh / 10 * 3) * -1; // s
+    let opacity = Math.random();
+
+    rule += `
+      .${snowflakeName}:nth-child(${i}) {
+        opacity: ${opacity};
+        transform: translate(${randomX}vw, -10px) scale(${randomScale});
+        animation: fall-${i} ${fallDuration}s ${fallDelay}s linear infinite;
+      }
+      @keyframes fall-${i} {
+        ${randomYoyoTime * 100}% {
+          transform: translate(${randomXEnd}vw, ${randomYoyoY}vh) scale(${randomScale});
+        }
+        to {
+          transform: translate(${randomXEndYoyo}vw, ${pageHeightVh}vh) scale(${randomScale});
+        }
+      }
+    `
+  }
+  addCss(rule);
+}
+
+// Load the rules and execute after the DOM loads
+createSnow = function () {
+  setHeightVariables();
+  getSnowAttributes();
+  spawnSnowCSS(snowflakesCount);
+  spawnSnow(snowflakesCount);
+};
+
+
+// export createSnow function if using node or CommonJS environment
+if (typeof module !== 'undefined') {
+  module.exports = {
+    createSnow,
+    showSnow,
+  };
+}
+else {
+  window.onload = createSnow;
+}
+
+// TODO add option to easily re-render scenery. For example when window resizes.
+// this should be easy as CSS rerenders after display block -> none -> block;
+// TODO add progress bar for slower clients
+let daysitem=document.querySelector("#days");
+let hoursitem=document.querySelector("#hours");
+let minitem=document.querySelector("#min");
+let secitem=document.querySelector("#sec");
+
+let countDown =()=>{
+  let futureDate = new Date("1 jan 2023")
+  let currentDate=new Date();
+  let mydate=futureDate-currentDate;
+  // console.log(mydate)
+  let days=Math.floor(mydate/1000/60/60/24);
+  let hours=Math.floor(mydate/1000/60/60)%24;
+  let min=Math.floor(mydate/1000/60)%60;
+  let sec=Math.floor(mydate/1000)%60;
+
+  daysitem.innerHTML=days;
+  hoursitem.innerHTML=hours;
+  minitem.innerHTML=min;
+  secitem.innerHTML=sec;
+}
+countDown()
+setInterval(countDown,1000)
+ 
+ 
